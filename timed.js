2e58a9d8ef8fe2ba5270f2c40e842e8be314360b
ChiperSoft <chiper@chipersoft.com>
2011-05-16 13:29:33 -0700

    File size reduction steps: - Merged sub-functions into main create_timer function. Unnecessary abstraction - Removed quotes around valid_types contents - Parsing of units from a 2 argument call now strips all non-alpha characters instead of relying on a float formatting to a string properly. - Switched to Google Closure Compiler for minified source - Other minor tweaks to save characters where possible.

--- timed.js
+++ timed.js
@@ -1,6 +1,7 @@
 /**
- * @fileOverview Timed
+ * Timed
  * Copyright (c) 2011 Jarvis Badgley, Arthur Klepchukov
+ * https://github.com/ChiperSoft/Timed
  * Licensed under the BSD license (BSD_LICENSE.txt)
  *
  * @author <a href="mailto:chipersoft@gmail.com">Jarvis Badgley</a>
@@ -26,135 +27,42 @@
 		hr	= min	* 60,
 		day = hr	* 24;
 			
-	/**
-	 * The supported units of time:<br />
-	 *	millisecond, milliseconds, ms,<br />
-	 *	second, seconds, sec, secs, s,<br />
-	 *	minute, minutes, min, mins, m,<br />
-	 *	hour, hours, hr, hrs, h,<br />
-	 *	day, days, d
-	 * @constant
-	 */
+	//supported unit formats.
 	var valid_units = {
-		"millisecond"	: ms,
-		"milliseconds"	: ms,
-		"ms"			: ms,
+		millisecond		: ms,
+		milliseconds	: ms,
+		ms				: ms,
 
-		"second"		: sec,
-		"seconds"		: sec,
-		"sec"			: sec,
-		"secs"			: sec,
-		"s"				: sec,
+		second			: sec,
+		seconds			: sec,
+		sec				: sec,
+		secs			: sec,
+		s				: sec,
 
-		"minute"		: min,
-		"minutes"		: min,
-		"min"			: min,
-		"mins"			: min,
-		"m"				: min,
+		minute			: min,
+		minutes			: min,
+		min				: min,
+		mins			: min,
+		m				: min,
 
-		"hour"			: hr,
-		"hours"			: hr,
-		"hr"			: hr,
-		"hrs"			: hr,
-		"h"				: hr,
+		hour			: hr,
+		hours			: hr,
+		hr				: hr,
+		hrs				: hr,
+		h				: hr,
 
-		"day"			: day,
-		"days"			: day,
-		"d"				: day
+		day				: day,
+		days			: day,
+		d				: day
 	};
-	
-	/**
-	 * Parses a numerical delay from the given arguments.
-	 * 
-	 * @param {Object} parsed The arguments parsed so far
-	 * @param {arguments} args The original arguments from the caller
-	 *	(e.g. {@link create_timer})
-	 * @throws Exception if the delay is not a number
-	 * @returns {Object} The parsed parameter updated with the parsed delay
-	 */
-	function parse_delay(parsed, args) {
-		if (typeof args[0] === "string") {
-			parsed.delay = parseFloat(args[0], 10);
-		} else {
-			parsed.delay = args[0];
-		}
-		
-		if (typeof parsed.delay !== "number" || isNaN(parsed.delay)) {
-			throw ("Timed.after and Timed.every - Require a numerical delay as the 1st argument");
-		}
-		
-		return parsed;
-	}
-
-	/**
-	 * Parses a units string from the given arguments.
-	 * 
-	 * @param {Object} parsed The arguments parsed so far
-	 * @param {arguments} args The original arguments from the caller
-	 *	(e.g. {@link create_timer})
-	 * @throws Exception if the units are not a key of {@link valid_units}
-	 * @returns {Object} The parsed parameter updated with the parsed units
-	 */
-	function parse_units(parsed, args) {
-		if (typeof args[0] === "string" && parsed.delay !== null) {
-			parsed.units = args[0].replace(parsed.delay, "") || null; // "9.7sec" || "9.7"
-		}
-		if (typeof args[1] === "string") {
-			parsed.units = args[1];
-		}
-		if (parsed.units === null && args.length === 2) { // no units specified
-			parsed.units = defaults.units;
-		}
-		
-		if (typeof valid_units[parsed.units] !== "number") {
-			throw ("Timed.after and Timed.every - Require a valid unit of time as the 2nd argument");
-		}
-		
-		return parsed;
-	}
-	
-	/**
-	 * Parses a callback function from the given arguments.
-	 * 
-	 * @param {Object} parsed The arguments parsed so far
-	 * @param {arguments} args The original arguments from the caller
-	 *	(e.g. {@link create_timer})
-	 * @throws Exception if the callback is not a function
-	 * @returns {Object} The parsed parameter updated with the parsed callback
-	 */
-	function parse_callback(parsed, args) {
-		parsed.callback = args[args.length - 1];
-		
-		if (typeof parsed.callback !== 'function') {
-			throw ("Timed.after and Timed.every - Require a callback as the last argument");
-		}
-		
-		return parsed;
-	}
 	
 	
 	/**
 	 * Accepts more human-readable arguments for creating JavaScript timers and 
 	 * converts them to values that can be inspected and passed along to 
-	 * setTimeout or setInterval.<br />
+	 * setTimeout or setInterval.
 	 * If the time when the timer should run is negative or faster than 
-	 * the default ({@link defaults}), 
-	 * it uses the default delay and default units.
-	 *
-	 * @param {Number|String} delay|delay+units 
-	 *	Combined with units, represents when a timer should run.<br />
-	 *	Units can be specified as part of this argument as a suffix of the string and 
-	 *	must represent a valid unit of time ({@link valid_units}).
-	 * @param {String} [units] 
-	 *	Combined with the delay, represents when a timer should run.
-	 *	If present, must be a valid unit of time ({@link valid_units}).
-	 * @param {Function} callback 
-	 *	Represents the code to be executed when the timer is ready.
-	 * 
-	 * @returns {Object} An object with a valid "delay", a valid "units" string, 
-	 *	a time, in milliseconds, of "when" the timer should run, and 
-	 *	a "callback" that the timer should execute when it's ready.
-	 * @static
+	 * the default it uses the default delay and default units.
 	 */
 	function create_timer() {
 		var parsed = {
@@ -162,15 +70,37 @@
 			units : null,
 			when : null,
 			callback : null
-		};
+		}, ac = arguments.length;
 	
-		if (arguments.length < 2 || arguments.length > 3) {
-			throw ("Timed.after and Timed.every - Accept only 2 or 3 arguments");
+		if (ac < 2 || ac > 3) throw ("Timed.after and Timed.every - Accept only 2 or 3 arguments");
+		
+		
+		//parse callback function
+		parsed.callback = arguments[ac - 1];
+		if (typeof parsed.callback !== 'function') throw ("Timed.after and Timed.every - Require a callback as the last argument");
+		
+	
+	
+		//parse delay field
+		parsed.delay = (typeof arguments[0] === "string") ? parseFloat(arguments[0], 10) : arguments[0];
+		if (typeof parsed.delay !== "number" || isNaN(parsed.delay)) throw ("Timed.after and Timed.every - Require a numerical delay as the 1st argument");
+
+
+
+		//parse units field
+		if (typeof arguments[0] === "string" && parsed.delay !== null) { //find units in "50sec" style delays
+			parsed.units = arguments[0].replace(/[^a-z]*/, "") || null;
 		}
-	
-		parsed = parse_delay(parsed, arguments);
-		parsed = parse_units(parsed, arguments);
-		parsed = parse_callback(parsed, arguments);
+		if (typeof arguments[1] === "string") {
+			parsed.units = arguments[1];
+		}
+		if (parsed.units === null && ac === 2) { // no units specified
+			parsed.units = defaults.units;
+		}
+		
+		if (typeof valid_units[parsed.units] !== "number") throw ("Timed.after and Timed.every - Require a valid unit of time as the 2nd argument");		
+
+
 	
 		// Reset to defaults, if necessary
 		if (parsed.delay < defaults.delay && parsed.units === defaults.units) {
@@ -190,21 +120,17 @@
 	var Timed = {
 		/**
 		 * Syntactic sugar for setTimeout.
-		 * <pre>
 		 *		setTimeout(function() { ... }, 300000); // becomes:
 		 *		Timed.after(5, "minutes", function() { ... });
-		 *		
+		 *
 		 *		// other valid calls:
 		 *		Timed.after(100, function() { ... }); // 100 milliseconds
 		 *		Timed.after("9.7", function() { ... }); // 9.7 milliseconds
 		 *		Timed.after("50sec", function() { ... }); // 50 seconds
 		 *		Timed.after("33", "hours", function() { ... }); // 33 hours
-		 * </pre>
-		 * Valid time units include: 
-		 * <strong>millisecond, second, minute, hour, & day</strong><br />
-		 * along with all their common abbreviations and pluralizations.<br />
-		 * (See full list of valid time units: {@link valid_units})
-		 * @name Timed.after
+		 *
+		 * Valid time units include millisecond, second, minute, hour, & day
+		 * along with all their common abbreviations and pluralizations.
 		 */
 		after : function after() {
 			var timer = create_timer.apply(this, arguments);
@@ -213,12 +139,11 @@
 
 		/**
 		 * Syntactic sugar for setTimeout.
-		 * <pre>
+		 *
 		 *		setInterval(function() { ... }, 300000); // becomes:
 		 *		Timed.every(5, "minutes", function() { ... });
-		 * </pre>
-		 * Supports the same syntax and arguments as {@link Timed.after}
-		 * @name Timed.every
+		 *
+		 * Supports the same syntax and arguments as Timed.after
 		 */
 		every : function every() {
 			var timer = create_timer.apply(this, arguments);
